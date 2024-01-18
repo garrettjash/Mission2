36b7623cf42a23b69f84548606e468cca8b13e54
garrettjash <garrettashcroft@gmail.com>
2024-01-17 17:39:58 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Garrett Ashcroft - Section 002
+// Mission 2
+// Program that simulates the rolling of 2 dice
+
+//Program.cs
+
+using Mission2;
+using System;
+internal class Program
+{
+
+    public static void Main(string[] args)
+    {
+        // Create a new instance of the rollDice class
+        RollDice rd = new RollDice();
+
+        // Get user input 
+        System.Console.WriteLine("Welcome to the dice throwing simulator!  \r\nHow many dice rolls would you like to simulate?");
+
+        // Set the input to the rolls variable 
+        int rolls = int.Parse(System.Console.ReadLine());
+
+        // Variable assigned to the array reurned from the rolls method
+        double[] sOutput = rd.Roll(rolls);
+
+
+        // Print the given output
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + rolls);
+
+        // For loop that prints the combinations 2-12 and accesses each element of the array
+        for (int iCount = 0; iCount < sOutput.Length; iCount++)
+        {
+            // One asterisk per percent, not one per roll***
+            // Get the percentage from the array
+            int percentage = (int)System.Math.Round((double)sOutput[iCount]);
+
+            // Add an aserisk for each percentage
+            string asterisks = new string('*', percentage);
+
+            // Print the histogram in the console
+            System.Console.WriteLine($"{iCount + 2}: {asterisks}");
+        }
+        System.Console.WriteLine("\r\nThank you for using the dice throwing simulator.  Goodbye!");
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,50 @@
+﻿// Garrett Ashcroft - Section 002
+// Mission 2
+// Program that simulates the rolling of 2 dice
+
+// RollDie.cs
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class RollDice
+    {
+        public double[] Roll(int rolls) // Constructor is only run one time, the first time the class is instantiated
+        {
+            // Import random
+            Random random = new Random();
+
+            // Create an empty array expecting a decimal
+            double[] aResults = new double[11];
+
+            // For loop that rolls dice for each time the user entered
+            for (int iOuter = 0; iOuter < rolls; iOuter++)
+            {
+                // Assigns both dice a 1-6 value
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                // Sums the roll
+                int rollSum = dice1 + dice2;
+
+                // Has a count of each time a combination is rolled 2-12
+                aResults[rollSum - 2] += 1;
+            }
+
+            // For loop that changes the count into a percentage
+            for (int iInner = 0; iInner < aResults.Length; iInner++) 
+            {
+                // Changes value in the array to the percentage
+                double timesRolled = aResults[iInner];
+                aResults[iInner] = timesRolled/rolls * 100;
+            }
+            // Returns the array
+            return aResults;
+        }
+    }
+}
